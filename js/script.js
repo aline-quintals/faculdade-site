d6788c8edc255741f9d70b3678ccda6a218c0acf
Alinequintals <aline.lima293@etec.sp.gov.br>
2026-05-30 12:30:34 -0300

    Primeira versão do portal de estudos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const semestre = document.getElementById("semestre");
+
+semestre.addEventListener("change", () => {
+
+    const pagina = semestre.value;
+
+    if (pagina !== "") {
+        window.location.href = pagina;
+    }
+
+});
